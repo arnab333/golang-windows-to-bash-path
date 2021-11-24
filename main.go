1a055395fa9159d8e2c70599a36f9dbc180623b3
arnab <arnab>
2021-11-24 14:06:57 +0530

    updated code

--- main.go
+++ main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-	"flag"
+	"fmt"
 	"strings"
 
 	"golang.design/x/clipboard"
 )
 
 func main() {
-	inputPath := flag.String(`path`, `C:\Users`, `provide a path from windows file system to convert them to bash format`)
-
-	flag.Parse()
-
-	bashPath := strings.Replace(*inputPath, `\`, `/`, -1)
+	fmt.Print("Enter Windows Path: ")
+	var input string
+	fmt.Scanln(&input)
+	bashPath := strings.Replace(input, `\`, `/`, -1)
 	clipboard.Write(clipboard.FmtText, []byte(bashPath))
 }
