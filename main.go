49f9052e000f7ada7044069ebb00f247586658b7
arnab <arnab>
2021-12-02 17:01:21 +0530

    bug fix for space

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"unicode"
 
 	"golang.design/x/clipboard"
 )
@@ -12,11 +13,18 @@ import (
 func main() {
 	fmt.Print("Enter Windows Path: ")
 	var input string
+	var pathArr []string
 	scanner := bufio.NewScanner(os.Stdin)
 	if scanner.Scan() {
 		input = scanner.Text()
 		bashPath := strings.Replace(input, `\`, `/`, -1)
-		clipboard.Write(clipboard.FmtText, []byte(bashPath))
+		for i := 0; i < len(bashPath); i++ {
+			pathArr = append(pathArr, string(bashPath[i]))
+			if unicode.IsSpace(rune(bashPath[i])) {
+				pathArr[i] = strings.Replace(string(bashPath[i]), ` `, `\ `, -1)
+			}
+		}
+		clipboard.Write(clipboard.FmtText, []byte(strings.Join(pathArr, "")))
 	}
 
 }
